ee6149991a938ad7b31b21d1ea9106b0e5f520d3
Scorpion96522 <126128140+Scorpion96522@users.noreply.github.com>
2023-05-20 13:44:47 +0300

    js/response Form el

--- Git API/js/main.js
+++ Git API/js/main.js
@@ -1,1 +1,27 @@
-const mainEl = document.querySelector('main');
+const mainEl = document.querySelector('.main');
+
+const formEl =document.createElement('form');
+formEl.classList.add('search');
+formEl.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const inputsValue = Object.fromEntries( new FormData(e.target));
+    const response = await fetch(`
+    https://api.github.com/users/${inputsValue.name}
+    `);
+
+    console.log(response)
+})
+
+const inputEl = document.createElement('input');
+inputEl.classList.add('search-input');
+inputEl.setAttribute('name', 'name');
+
+const searchButtonEl = document.createElement('button');
+searchButtonEl.classList.add('search-button');
+searchButtonEl.setAttribute('type', 'submit');
+searchButtonEl.innerHTML = "Поиск";
+
+formEl.appendChild(inputEl);
+formEl.appendChild(searchButtonEl);
+mainEl.appendChild(formEl);
+
